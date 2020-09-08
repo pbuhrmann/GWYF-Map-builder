4a78e5c81297d00945e0316118a722259dd53d20
penrique <penrique@soflex.com.ar>
2020-09-08 02:46:50 -0300

    console args

--- index.js
+++ index.js
@@ -2,6 +2,16 @@
 exports.__esModule = true;
 var generator_1 = require("./generator");
 var fs = require("fs");
-var mazebuilder = new generator_1.MazeBuilder('Auto-gen Maze', 12, 12);
+var args = process.argv.slice(2);
+var name = args[0] ? args[0] : 'My Maze';
+var width = args[1] ? args[1] : "8";
+var height = args[2] ? args[2] : "8";
+var holes = args[3] ? args[3] : "18";
+var home_location = process.env.HOME || process.env.USERPROFILE;
+var mazebuilder = new generator_1.MazeBuilder(name, parseInt(width), parseInt(height));
 fs.writeFileSync('Map', mazebuilder.build());
+fs.copyFile('Map', home_location + "/AppData/LocalLow/Team17 Digital Ltd/Golf With Your Friends/CustomLevels/" + name + "/Map", function (err) {
+    if (err)
+        throw err;
+});
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,5 +1,19 @@
 import {  MazeBuilder } from './generator'
 import * as fs from 'fs';
 
-const mazebuilder = new MazeBuilder('Auto-gen Maze', 12, 12);
+const args = process.argv.slice(2);
+
+const name = args[0] ? args[0] : 'My Maze';
+const width = args[1] ? args[1] : "8";
+const height = args[2] ? args[2] : "8";
+const holes = args[3] ? args[3] : "18";
+
+const home_location =  process.env.HOME || process.env.USERPROFILE;
+
+const mazebuilder = new MazeBuilder(name, parseInt(width), parseInt(height));
 fs.writeFileSync('Map', mazebuilder.build());
+// fs.writeFileSync('Map', mazebuilder.buildMultiple(parseInt(holes)));
+
+fs.copyFile('Map', `${home_location}/AppData/LocalLow/Team17 Digital Ltd/Golf With Your Friends/CustomLevels/${name}/Map`, (err) => {
+    if (err) throw err;
+});
