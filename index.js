dfd41a042090c524fc04672e8055dc73e696624c
penrique <penrique@soflex.com.ar>
2020-09-11 17:53:07 -0300

    type arg

--- index.js
+++ index.js
@@ -4,13 +4,27 @@ var generator_1 = require("./generator");
 var fs = require("fs");
 var args = process.argv.slice(2);
 var name = args[0] ? args[0] : 'My Maze';
-var width = args[1] ? args[1] : "8";
-var height = args[2] ? args[2] : "8";
-var holes = args[3] ? args[3] : "18";
+var type = args[1] ? args[1] : 'single';
+var width = args[2] ? args[2] : "8";
+var height = args[3] ? args[3] : "8";
+var holes = args[4] ? args[4] : "18";
 var home_location = process.env.HOME || process.env.USERPROFILE;
 var mazebuilder = new generator_1.MazeBuilder(name, parseInt(width), parseInt(height));
-var custom = fs.readFileSync('CustomMap.txt', 'utf8');
-fs.writeFileSync('Map', mazebuilder.buildFromFile(custom));
+switch (type) {
+    case 'single':
+        fs.writeFileSync('Map', mazebuilder.build());
+        break;
+    case 'multi':
+        fs.writeFileSync('Map', mazebuilder.buildMultiple(parseInt(holes)));
+        break;
+    case 'custom':
+        var custom = fs.readFileSync('CustomMap.txt', 'utf8');
+        fs.writeFileSync('Map', mazebuilder.buildFromFile(custom));
+        break;
+    default:
+        fs.writeFileSync('Map', mazebuilder.build());
+        break;
+}
 fs.copyFile('Map', home_location + "/AppData/LocalLow/Team17 Digital Ltd/Golf With Your Friends/CustomLevels/" + name + "/Map", function (err) {
     if (err)
         throw err;
--- index.ts
+++ index.ts
@@ -1,22 +1,33 @@
-import {  MazeBuilder } from './generator'
+import { MazeBuilder } from './generator'
 import * as fs from 'fs';
 
 const args = process.argv.slice(2);
 
 const name = args[0] ? args[0] : 'My Maze';
-const width = args[1] ? args[1] : "8";
-const height = args[2] ? args[2] : "8";
-const holes = args[3] ? args[3] : "18";
+const type = args[1] ? args[1] : 'single';
+const width = args[2] ? args[2] : "8";
+const height = args[3] ? args[3] : "8";
+const holes = args[4] ? args[4] : "18";
 
-const home_location =  process.env.HOME || process.env.USERPROFILE;
+const home_location = process.env.HOME || process.env.USERPROFILE;
 
 const mazebuilder = new MazeBuilder(name, parseInt(width), parseInt(height));
 
-// fs.writeFileSync('Map', mazebuilder.build());                        // Single Maze
-// fs.writeFileSync('Map', mazebuilder.buildMultiple(parseInt(holes))); // Multiple Mazes
-
-const custom = fs.readFileSync('CustomMap.txt', 'utf8');                // Build from file
-fs.writeFileSync('Map', mazebuilder.buildFromFile(custom));             // Build from file
+switch (type) {
+    case 'single':
+        fs.writeFileSync('Map', mazebuilder.build());                           // Single Maze
+        break;
+    case 'multi':
+        fs.writeFileSync('Map', mazebuilder.buildMultiple(parseInt(holes)));    // Multiple Mazes
+        break;
+    case 'custom':
+        const custom = fs.readFileSync('CustomMap.txt', 'utf8');                // Build from file
+        fs.writeFileSync('Map', mazebuilder.buildFromFile(custom));             // Build from file
+        break;
+    default:
+        fs.writeFileSync('Map', mazebuilder.build());
+        break;
+}
 
 fs.copyFile('Map', `${home_location}/AppData/LocalLow/Team17 Digital Ltd/Golf With Your Friends/CustomLevels/${name}/Map`, (err) => {
     if (err) throw err;
