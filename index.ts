50631a80b8b8016da65d8939a0b8ec2890e6636b
penrique <penrique@soflex.com.ar>
2020-09-11 17:21:49 -0300

    comments

--- index.ts
+++ index.ts
@@ -11,11 +11,12 @@ const holes = args[3] ? args[3] : "18";
 const home_location =  process.env.HOME || process.env.USERPROFILE;
 
 const mazebuilder = new MazeBuilder(name, parseInt(width), parseInt(height));
-// fs.writeFileSync('Map', mazebuilder.build());
-// fs.writeFileSync('Map', mazebuilder.buildMultiple(parseInt(holes)));
 
-const custom = fs.readFileSync('CustomMap.txt', 'utf8');
-fs.writeFileSync('Map', mazebuilder.buildFromFile(custom));
+// fs.writeFileSync('Map', mazebuilder.build());                        // Single Maze
+// fs.writeFileSync('Map', mazebuilder.buildMultiple(parseInt(holes))); // Multiple Mazes
+
+const custom = fs.readFileSync('CustomMap.txt', 'utf8');                // Build from file
+fs.writeFileSync('Map', mazebuilder.buildFromFile(custom));             // Build from file
 
 fs.copyFile('Map', `${home_location}/AppData/LocalLow/Team17 Digital Ltd/Golf With Your Friends/CustomLevels/${name}/Map`, (err) => {
     if (err) throw err;
