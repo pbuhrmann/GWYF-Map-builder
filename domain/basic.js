7237e67b1e8112d9a30fbab07e0673844a8aca0c
penrique <penrique@soflex.com.ar>
2020-09-25 18:55:02 -0300

    ignore generated js files

--- domain/basic.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var Basic = (function () {
-    function Basic() {
-    }
-    return Basic;
-}());
-exports.Basic = Basic;
-//# sourceMappingURL=basic.js.map
--- generator/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-exports.__esModule = true;
-__export(require("./maze"));
-__export(require("./generate"));
-__export(require("./mazebuilder"));
-//# sourceMappingURL=index.js.map
