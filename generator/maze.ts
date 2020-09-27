dbcd6515cf50305420d7a0ac6655781dca62d908
Barbara Martinez <barbara.martinez@southworks.com>
2020-09-26 21:06:06 -0300

    Refactor - Code cleanup and catch missing dir err

--- generator/maze.ts
+++ generator/maze.ts
@@ -1,33 +1,33 @@
-export function maze(x: number, y: number) {
+export const maze = (x: number, y: number) => {
 	let n = x * y - 1;
 
 	if (n < 0) { console.error("illegal maze dimensions"); return; }
 
-	let horiz = []; for (var j = 0; j < x; j++) horiz[j] = [];
-	let verti = []; for (var j = 0; j < x; j++) verti[j] = [];
+	let horiz = []; for (let j = 0; j < x; j++) horiz[j] = [];
+	let verti = []; for (let j = 0; j < x; j++) verti[j] = [];
 	let here = [Math.floor(Math.random() * x), Math.floor(Math.random() * y)];
 	let path = [here];
 	let unvisited = [];
 
-	for (var j = 0; j < x + 2; j++) {
+	for (let j = 0; j < x + 2; j++) {
 		unvisited[j] = [];
-		for (var k = 0; k < y + 1; k++)
+		for (let k = 0; k < y + 1; k++)
 			unvisited[j].push(j > 0 && j < x + 1 && k > 0 && (j != here[0] + 1 || k != here[1] + 1));
 	}
 
 	while (0 < n) {
-		var potential = [[here[0] + 1, here[1]], [here[0], here[1] + 1],
+		let potential = [[here[0] + 1, here[1]], [here[0], here[1] + 1],
 		[here[0] - 1, here[1]], [here[0], here[1] - 1]];
-		var neighbors = [];
-		
-		for (var j = 0; j < 4; j++) {
+		let neighbors = [];
+
+		for (let j = 0; j < 4; j++) {
 			if (unvisited[potential[j][0] + 1][potential[j][1] + 1])
 				neighbors.push(potential[j]);
 		}
 
 		if (neighbors.length) {
 			n = n - 1;
-			let next = neighbors[Math.floor(Math.random() * neighbors.length)];
+			let next = neighbors[~~(Math.random() * neighbors.length)];
 			unvisited[next[0] + 1][next[1] + 1] = false;
 			if (next[0] == here[0])
 				horiz[next[0]][(next[1] + here[1] - 1) / 2] = true;
@@ -40,21 +40,21 @@ export function maze(x: number, y: number) {
 	return { x: x, y: y, horiz: horiz, verti: verti };
 }
 
-export function display(m) {
-	var text = [];
-	for (var j = 0; j < m.x * 2 + 1; j++) {
-		var line = [];
+export const display = (m) => {
+	let text = [];
+	for (let j = 0; j < m.x * 2 + 1; j++) {
+		let line = [];
 		if (0 == j % 2)
-			for (var k = 0; k < m.y * 4 + 1; k++)
+			for (let k = 0; k < m.y * 4 + 1; k++)
 				if (0 == k % 4)
 					line[k] = '+';
 				else
-					if (j > 0 && m.verti[j / 2 - 1][Math.floor(k / 4)])
+					if (j > 0 && m.verti[j / 2 - 1][~~(k / 4)])
 						line[k] = '#';
 					else
 						line[k] = '-';
 		else
-			for (var k = 0; k < m.y * 4 + 1; k++)
+			for (let k = 0; k < m.y * 4 + 1; k++)
 				if (0 == k % 4)
 					if (k > 0 && m.horiz[(j - 1) / 2][k / 4 - 1])
 						line[k] = '#';
@@ -66,5 +66,6 @@ export function display(m) {
 		if (m.x * 2 - 1 == j) line[4 * m.y] = '#';
 		text.push(line.join('') + '\r\n');
 	}
+
 	return text.join('');
 }
--- generator/mazebuilder.ts
+++ generator/mazebuilder.ts
@@ -1,4 +1,4 @@
-import { Basic, WallH, WallV, Floor, Spawn, HoleIndent, Flagpole, FlipGate } from "../domain";
+import { Basic, WallH, WallV, Floor, Spawn, HoleIndent, Flagpole } from "../domain";
 
 // Map limits: 
 // -245X, 245Z TL
@@ -16,6 +16,7 @@ export class MazeBuilder {
 
     private x: number;
     private y: number;
+    private map_boilerplate: string = `{levelName: {0}, description: Automatically generated maze, publishedID: 0, music : 8, skybox : 9, editorObjectData : [{1}]`
 
     constructor(name: string, width: number, height: number) {
         this.name = name;
@@ -24,11 +25,11 @@ export class MazeBuilder {
     }
 
     public build() {
-        const m = this.generateMaze(this.width, this.height);
-        console.log(this.generateDisplay(m));
-        const generated_map = this.generateFromDisplay(this.generateDisplay(m));
+        const maze = this.generateMaze(this.width, this.height);
+        console.log(this.generateDisplay(maze));
+        const generated_map = this.generateFromDisplay(this.generateDisplay(maze));
 
-        return `{"levelName": "${this.name}","description": "Automatically generated maze","publishedID": 0,"music": 8,"skybox": 9,"editorObjectData": [${generated_map}]}`;
+        return this.map_boilerplate.replace("{0}", this.name).replace("{1}", generated_map);
     }
 
     public buildMultiple(holes_total: number) {
@@ -39,9 +40,9 @@ export class MazeBuilder {
         this.y = 250;
 
         for (let i = 0; i < holes_total; i++) {
-            const m = this.generateMaze(this.width, this.height);
-            console.log(this.generateDisplay(m));
-            generated_map += this.generateFromDisplay(this.generateDisplay(m), this.x, this.y);
+            const maze = this.generateMaze(this.width, this.height);
+            console.log(this.generateDisplay(maze));
+            generated_map += this.generateFromDisplay(this.generateDisplay(maze), this.x, this.y);
 
             this.y += real_height + 3;
             this.x += 3;
@@ -60,7 +61,7 @@ export class MazeBuilder {
             }
         }
 
-        return `{"levelName": "${this.name}","description": "Automatically generated maze","publishedID": 0,"music": 8,"skybox": 9,"editorObjectData": [${generated_map}]}`;
+        return this.map_boilerplate.replace("{0}", this.name).replace("{1}", generated_map);
     }
 
     public buildFromFile(map: string) {
@@ -71,15 +72,15 @@ export class MazeBuilder {
         console.log(map.split('\n').length, this.height);
         console.log(map);
         const generated_map = this.generateFromDisplay(map);
-        return `{"levelName": "${this.name}","description": "Automatically generated maze","publishedID": 0,"music": 8,"skybox": 9,"editorObjectData": [${generated_map}]}`;
+        return this.map_boilerplate.replace("{0}", this.name).replace("{1}", generated_map);
     }
 
     generateFromDisplay(display: string, x?: number, y?: number) {
         const result: Basic[] = [];
         const lines = display.split('\r\n');
-        
-        this.x = x !== undefined ? x : Math.floor(-this.width * 6 / 2);
-        this.y = y !== undefined ? y : Math.floor(this.height * 6 / 2);
+
+        this.x = x !== undefined ? x : ~~(-this.width * 6 / 2);
+        this.y = y !== undefined ? y : ~~(this.height * 6 / 2);
 
         for (let i = 0; i < lines.length - 1; i++) {
             const line = lines[i];
@@ -92,14 +93,13 @@ export class MazeBuilder {
             }
 
             for (let j = 0; j < line.length - 1; j = j + 4) {
+
                 if (i % 2 == 0) {
                     const sub = line.substr(j, 5);
+
                     if (sub == '+---+') {
                         const wall = new WallH(this.x, this.y - 3);
                         result.push(wall);
-                    } else if (sub == '+###+') {
-                        // const flipgate = new FlipGate(this.x, this.y - 3, 'H');
-                        // result.push(flipgate);
                     }
 
                     if (i == lines.length - 2 && j == line.length - 5) {
@@ -110,6 +110,7 @@ export class MazeBuilder {
                         const floor = new Floor(this.x, this.y);
                         result.push(floor);
                     }
+
                 }
                 else {
                     const sub = line.substr(j, 5);
@@ -121,16 +122,13 @@ export class MazeBuilder {
                     }
                     else if (sub == '|####') {
                         const wall = new WallV(this.x - 3, this.y - 3);
-                        // const flipgate = new FlipGate(this.x + 3, this.y - 3, 'V');
                         result.push(wall);
                     }
                     else if (sub == '####|') {
                         const wall = new WallV(this.x + 3, this.y - 3);
                         result.push(wall);
-                    } else if (sub == '#####') {
-                        // const flipgate = new FlipGate(this.x + 3, this.y - 3, 'V');
-                        // result.push(flipgate);
                     }
+
                 }
                 this.x += 6;
 
@@ -147,31 +145,31 @@ export class MazeBuilder {
 
         if (n < 0) { console.error("illegal maze dimensions"); return; }
 
-        let horiz = []; for (var j = 0; j < x; j++) horiz[j] = [];
-        let verti = []; for (var j = 0; j < x; j++) verti[j] = [];
-        let here = [Math.floor(Math.random() * x), Math.floor(Math.random() * y)];
+        let horiz = []; for (let j = 0; j < x; j++) horiz[j] = [];
+        let verti = []; for (let j = 0; j < x; j++) verti[j] = [];
+        let here = [~~(Math.random() * x), ~~(Math.random() * y)];
         let path = [here];
         let unvisited = [];
 
-        for (var j = 0; j < x + 2; j++) {
+        for (let j = 0; j < x + 2; j++) {
             unvisited[j] = [];
-            for (var k = 0; k < y + 1; k++)
+            for (let k = 0; k < y + 1; k++)
                 unvisited[j].push(j > 0 && j < x + 1 && k > 0 && (j != here[0] + 1 || k != here[1] + 1));
         }
 
         while (0 < n) {
-            var potential = [[here[0] + 1, here[1]], [here[0], here[1] + 1],
+            let potential = [[here[0] + 1, here[1]], [here[0], here[1] + 1],
             [here[0] - 1, here[1]], [here[0], here[1] - 1]];
-            var neighbors = [];
+            let neighbors = [];
 
-            for (var j = 0; j < 4; j++) {
+            for (let j = 0; j < 4; j++) {
                 if (unvisited[potential[j][0] + 1][potential[j][1] + 1])
                     neighbors.push(potential[j]);
             }
 
             if (neighbors.length) {
                 n = n - 1;
-                let next = neighbors[Math.floor(Math.random() * neighbors.length)];
+                let next = neighbors[~~(Math.random() * neighbors.length)];
                 unvisited[next[0] + 1][next[1] + 1] = false;
                 if (next[0] == here[0])
                     horiz[next[0]][(next[1] + here[1] - 1) / 2] = true;
@@ -186,11 +184,11 @@ export class MazeBuilder {
 
     // taken from: http://rosettacode.org/wiki/Maze_generation
     generateDisplay(m) {
-        var text = [];
-        for (var j = 0; j < m.x * 2 + 1; j++) {
-            var line = [];
+        let text = [];
+        for (let j = 0; j < m.x * 2 + 1; j++) {
+            let line = [];
             if (0 == j % 2)
-                for (var k = 0; k < m.y * 4 + 1; k++)
+                for (let k = 0; k < m.y * 4 + 1; k++)
                     if (0 == k % 4)
                         line[k] = '+';
                     else
@@ -199,7 +197,7 @@ export class MazeBuilder {
                         else
                             line[k] = '-';
             else
-                for (var k = 0; k < m.y * 4 + 1; k++)
+                for (let k = 0; k < m.y * 4 + 1; k++)
                     if (0 == k % 4)
                         if (k > 0 && m.horiz[(j - 1) / 2][k / 4 - 1])
                             line[k] = '#';
@@ -207,10 +205,10 @@ export class MazeBuilder {
                             line[k] = '|';
                     else
                         line[k] = '#';
-            // if (0 == j) line[1] = line[2] = line[3] = '#';
-            // if (m.x * 2 - 1 == j) line[4 * m.y] = '#';
+
             text.push(line.join('') + '\r\n');
         }
+
         return text.join('');
     }
 }
