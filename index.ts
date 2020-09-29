50987bbb712787dba02d5737b30c8ea0e97b1d7c
penrique <penrique@soflex.com.ar>
2020-09-29 01:51:13 -0300

    yargs

--- index.ts
+++ index.ts
@@ -1,24 +1,77 @@
 import { MazeBuilder } from './generator'
 import * as fs from 'fs';
-
-const args = process.argv.slice(2);
-
-const name = args[0] ? args[0] : 'My Maze';
-const type = args[1] ? args[1] : 'single';
-const width = args[2] ? args[2] : '8';
-const height = args[3] ? args[3] : '8';
-const holes = args[4] ? args[4] : '18';
+import * as yargs from 'yargs';
+import { Global } from './config';
 
 const home_location = process.env.HOME || process.env.USERPROFILE;
 
-const mazebuilder = new MazeBuilder(name, parseInt(width), parseInt(height));
 
-switch (type) {
+const argv = yargs
+    .option('name', {
+        description: 'Map name',
+        type: 'string'
+    })
+    .option('type', {
+        description: 'single, tower, multi, custom (load from file)',
+        type: 'string'
+    })
+    .option('width', {
+        description: 'Width of map',
+        type: 'number'
+    })
+    .option('height', {
+        description: 'Height of map',
+        type: 'number'
+    })
+    .option('holes', {
+        description: 'Number of holes',
+        type: 'number'
+    })
+    .option('stories', {
+        description: 'Tower stories',
+        type: 'number'
+    })
+    .option('walls', {
+        description: 'Number of walls stacked on each other',
+        type: 'number'
+    })
+    .option('storyHeight', {
+        description: 'Height of each story (Tower only)',
+        type: 'number'
+    })
+    .option('basic', {
+        description: 'No spawns, no flagpoles',
+        type: 'boolean'
+    })
+    .alias('name', 'n')
+    .alias('type', 't')
+    .alias('width', 'w')
+    .alias('height', 'h')
+    .help().argv;
+
+if (argv.type)
+    Global.type = argv.type ? argv.type : 'single';
+
+if (argv.basic)
+    Global.basic = argv.basic ? true : false;
+
+if (argv.walls)
+    Global.wallStacks = argv.walls;
+
+if (argv.storyHeight)
+    Global.storyHeight = argv.storyHeight;
+
+const mazebuilder = new MazeBuilder(argv.name, argv.width, argv.height);
+
+switch (argv.type) {
     case 'single':
         fs.writeFileSync('Map', mazebuilder.build());                           // Single Maze
         break;
+    case 'tower':
+        fs.writeFileSync('Map', mazebuilder.buildTower(argv.stories));          // Tower
+        break;
     case 'multi':
-        fs.writeFileSync('Map', mazebuilder.buildMultiple(parseInt(holes)));    // Multiple Mazes
+        fs.writeFileSync('Map', mazebuilder.buildMultiple(argv.holes));         // Multiple Mazes
         break;
     case 'custom':
         const custom = fs.readFileSync('CustomMap.txt', 'utf8');                // Build from file
@@ -30,7 +83,7 @@ switch (type) {
 }
 
 try {
-    const dir = `${home_location}/AppData/LocalLow/Team17 Digital Ltd/Golf With Your Friends/CustomLevels/${name}`;
+    const dir = `${home_location}/AppData/LocalLow/Team17 Digital Ltd/Golf With Your Friends/CustomLevels/${argv.name}`;
     !fs.existsSync(dir) && fs.mkdirSync(dir);
 
     fs.copyFile('Map', `${dir}/Map`, err => {
