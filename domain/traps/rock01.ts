020ca42993a50e5ccedcab0d8bd076d8577c5516
penrique <penrique@soflex.com.ar>
2020-09-29 01:52:39 -0300

    traps

---
+++ domain/traps/rock01.ts
@@ -0,0 +1,15 @@
+import { Basic } from "../basic";
+
+export class Rock01 extends Basic {
+
+    constructor(x: number, y: number, z: number) {
+        super();
+        this.pX = x;
+        this.pZ = y;
+        this.pY = z;
+    }
+
+    toString() {
+        return `{"sType": 0,"pX": ${this.pX},"pY": ${this.pY},"pZ": ${this.pZ},"rW": 1.0,"rX": 0.0,"rY": 0.0,"rZ": 0.0,"sX": 1.0,"sY": 1.0,"sZ": 1.0,"obName": "FRock01","photonData": {"photonViewID": []}}`;
+    }
+}
