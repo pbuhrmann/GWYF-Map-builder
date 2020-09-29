c4a6b5b43431d20c3e6dc50295755d3e7940bd6f
penrique <penrique@soflex.com.ar>
2020-09-29 01:51:53 -0300

    new global properties

--- config/global.ts
+++ config/global.ts
@@ -1,3 +1,9 @@
 export class Global {
+    static theme: string = "Forest";
+    static type: string = "single";
     static y: number = 4.0;
+    static wallStacks: number = 1;
+    static storyHeight: number = 10;
+    static basic: boolean = false;
+    static trapProbability: number = 0.25;
 }
